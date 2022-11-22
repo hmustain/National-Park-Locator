0527fb93c4b9400ddb148ae56a5dc5562f89e67c
hmustain <hunter@bunkerbranding.com>
2022-11-21 21:38:37 -0600

    we have fixed the code to where the cards are now dynamically populating through the array

--- assets/script.js
+++ assets/script.js
@@ -85,10 +85,6 @@ var getStateCode = function (state) {
 };
 
 
-// Create the card
-var createParkCard = function(parkData) {
-
-}
 
 // Get Park listing
 var getParkList = function (abbr) {
@@ -100,7 +96,7 @@ var getParkList = function (abbr) {
         console.log(response);
         response.json().then(function (data) {
           console.log(data);
-          getInfo(data);
+          createParkCard(data);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -108,49 +104,99 @@ var getParkList = function (abbr) {
     })
     .catch(function (error) {
       alert('Unable to connect to National Park Service');
-    });
-
-
-
-  var getInfo = function (data) {
-    // park 1 card
-    var parkName = document.getElementById('park-name');
-    var parkDesc = document.getElementById('text-description');
-    var parkUrl = document.getElementById('park-url');
-    var parkImage = document.getElementById('park-image')
-
-// park 2 card
-    var parkName2 = document.getElementById('park-name2');
-    var parkDesc2 = document.getElementById('text-description2');
-    var parkUrl2 = document.getElementById('park-url2');
-    var parkImage2 = document.getElementById('park-image2')
-
-    // console.log(data.data[2].name);
-    // park 1 getting data to show up in browser
-    parkName.textContent = data.data[2].fullName;
-    parkDesc.textContent = data.data[2].description;
-    parkUrl.textContent = data.data[2].fullName;
-    parkUrl.href = data.data[2].url;
-    parkImage.src = data.data[2].images[0].url;
-
-    // console logs 
-    console.log(parkUrl);
-    // parkUrl.a = data.data[2].url;
-    console.log(parkImage2);
-    console.log(data.data[3].name);
-
-    //park 2 getting data to show up in browser
-    parkName2.textContent = data.data[3].fullName;
-    parkDesc2.textContent = data.data[3].description;
-    parkUrl2.textContent = data.data[3].fullName;
-    parkUrl2.href = data.data[3].url;
-    parkImage2.src = data.data[3].images[0].url;
-
-    // parkUrl2.textContent = data.data[3].url;
-    // parkUrl2.href = data.data[3].url;
-
-  };
+    })
 };
+
+// Create the card
+var createParkCard = function (parkData) {
+  for (let i = 0; i < parkData.data.length; i++) {
+
+    var parkContainer = document.getElementById('park-container');
+
+    var divRow = document.createElement('div');
+    divRow.classList = 'row';
+    parkContainer.appendChild(divRow);
+    var divCol = document.createElement('div');
+    divCol.classList = 'col s12 m7';
+    divRow.appendChild(divCol);
+
+    var divCard = document.createElement('div');
+    divCard.classList = 'card';
+    divCol.appendChild(divCard);
+
+    var parkImgDiv = document.createElement('div');
+    parkImgDiv.classList = 'card-image';
+    divCard.appendChild(parkImgDiv);
+
+    var parkImg = document.createElement('img');
+    parkImg.setAttribute('src', parkData.data[i].images[0].url);
+    parkImg.setAttribute('style', 'width:515px height:345px' );
+    parkImgDiv.appendChild(parkImg);
+
+    var parkName = document.createElement('span');
+    parkName.classList = 'card-title';
+    parkName.textContent = parkData.data[i].fullName;
+    parkImgDiv.appendChild(parkName);
+
+    var divText = document.createElement('div');
+    divText.classList = 'card-content';
+    divCard.appendChild(divText);
+
+    var parkDesc = document.createElement('p')
+    parkDesc.textContent = parkData.data[i].description;
+    divText.appendChild(parkDesc);
+
+    var parkLink = document.createElement('div');
+    parkLink.classList = 'card-action';
+    divCard.appendChild(parkLink);
+
+    var parkUrl = document.createElement('a')
+    parkUrl.setAttribute('href', parkData.data[i].url);
+    parkUrl.textContent = parkData.data[i].fullName + "Website";
+    parkLink.appendChild(parkUrl);
+
+  }
+};
+
+//   var getInfo = function (data) {
+//     // park 1 card
+//     var parkName = document.getElementById('park-name');
+//     var parkDesc = document.getElementById('text-description');
+//     var parkUrl = document.getElementById('park-url');
+//     var parkImage = document.getElementById('park-image')
+
+// // park 2 card
+//     var parkName2 = document.getElementById('park-name2');
+//     var parkDesc2 = document.getElementById('text-description2');
+//     var parkUrl2 = document.getElementById('park-url2');
+//     var parkImage2 = document.getElementById('park-image2')
+
+//     // console.log(data.data[2].name);
+//     // park 1 getting data to show up in browser
+//     parkName.textContent = data.data[2].fullName;
+//     parkDesc.textContent = data.data[2].description;
+//     parkUrl.textContent = data.data[2].fullName;
+//     parkUrl.href = data.data[2].url;
+//     parkImage.src = data.data[2].images[0].url;
+
+//     // console logs 
+//     console.log(parkUrl);
+//     // parkUrl.a = data.data[2].url;
+//     console.log(parkImage2);
+//     console.log(data.data[3].name);
+
+//     //park 2 getting data to show up in browser
+//     parkName2.textContent = data.data[3].fullName;
+//     parkDesc2.textContent = data.data[3].description;
+//     parkUrl2.textContent = data.data[3].fullName;
+//     parkUrl2.href = data.data[3].url;
+//     parkImage2.src = data.data[3].images[0].url;
+
+//     // parkUrl2.textContent = data.data[3].url;
+//     // parkUrl2.href = data.data[3].url;
+
+//   };
+// };
 
 // var getInfo = function (data) {
 // var parkName = document.getElementById('park-name');
